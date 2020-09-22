42b29ee99ecfd86c245a29c2d6d2fd0693e48d87
Dan Gibson <dgibson1124@gmail.com>
2020-09-22 14:39:35 -0700

    Feedback update
    - removed the park name search area
    - added styling
    - updated multiple state query since multiple states are returned by "," rather than new query parameters for each state.
    - Could not get err catch to work properly so set up .then to return the error message returned from that

--- app.js
+++ app.js
@@ -3,29 +3,28 @@
 const apiKey='60nlsIVM3HBtdQionDcOxEWoMsFQEBxqQpRRTpzJ'
 const url='https://developer.nps.gov/api/v1/parks?api_key=' + apiKey;
 
-// adds formatting for each new parameter after API key
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
   }
 
-// displays national parks along with link to .gov site
+
   function displayResults(responseJson){
     console.log(responseJson);
     $('#results').removeClass('hidden');
       $('#results-list').empty();
       for(let i = 0; i < responseJson.data.length; i++){
         $('#results-list').append(
-                `<ul>
+                `
                     <li>
                       <a href=${responseJson.data[i].url} target="_blank">
                       <h3>${responseJson.data[i].fullName}</h3></a>
                       <h4>${responseJson.data[i].description}</h4>
-                      <p>${responseJson.data[i].addresses[i].line1}
-                      <br> ${responseJson.data[i].addresses[i].city}, 
-                      ${responseJson.data[i].addresses[i].stateCode}   
-                      ${responseJson.data[i].addresses[i].postalCode}
+                      <p>${responseJson.data[i].addresses[1].line1}<br>
+                      ${responseJson.data[i].addresses[1].city}, 
+                      ${responseJson.data[i].addresses[1].stateCode}</p>
+                      ${responseJson.data[i].addresses[1].postalCode}
                     </li>
                 `
                 );
@@ -33,17 +32,20 @@ function formatQueryParams(params) {
   };
 
 
-  function getParksList(query, limit, stateArray) {
+  function getParksList(limit, stateArray) {
+    
       console.log(stateArray)
+      // for(let i = 0; i < stateArray.length; i++){
         const params = {
-            q: query,
-            limit
+            limit,
+            stateCode: stateArray
         };
+      // }
       const queryString = formatQueryParams(params)
       let finalUrl = url + '&' + queryString
-        for (let i = 0; i < stateArray.length; i++) {
-            finalUrl += `&stateCode=${stateArray[i]}`
-        }
+        // for (let i = 0; i < stateArray.length; i++) {
+        //     finalUrl += `&stateCode=${stateArray[i]}`
+        // }
     
 
       console.log(finalUrl)
@@ -53,11 +55,11 @@ function formatQueryParams(params) {
         if (response.ok) {
           return response.json();
         }
-        throw alert('nothing to see here');
+        throw Error(`looks like there was an issue: ${response.statusText}`);
       })
       .then(responseJson => displayResults(responseJson))
       .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        return alert(`${err.message}`);
       });
     }
 
@@ -66,10 +68,10 @@ function formatQueryParams(params) {
   function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
-      const searchTerm = $('#js-search-area').val();
+      // const searchTerm = $('#js-search-area').val();
       const maxResults = $('#js-max-results').val();
       const stateArr  = $('#js-multiple-states').val().split(" ")
-      getParksList(searchTerm, maxResults, stateArr);
+      getParksList(maxResults, stateArr);
     });
   }
 
